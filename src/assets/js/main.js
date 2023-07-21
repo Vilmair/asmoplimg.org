91383eb008aeb67e4a68c69871ab958c548c2334
Vilmair Medeiros <vilmair@phorts.com.br>
2023-07-20 21:10:47 -0300

    Main version

---
+++ src/assets/js/main.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+
+    $("#cpf").focusout(function(){
+        $('input[name="cpf"]').val( this.value.toUpperCase() );
+              //Início do Comando AJAX
+        console.log($("#cpf")[0].value, $("#cpf")[0]);
+    });
+
+(function($) {
+
+    // $('input[name="cpf"]').mask('000.000.000-00');
+    // $('input[name="cep"]').mask('00000-000');
+    // $('input[name="phone"]').mask('(00) 00000 0000');
+
+    $('#cep').on('focus', function() {
+        //$(this).toggleClass('input1 input2');
+    });
+
+})(jQuery);
+
+});
